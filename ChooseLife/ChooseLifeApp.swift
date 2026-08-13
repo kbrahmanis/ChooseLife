70ca3599124f5f5cdec638bcf70cc25906fcd279
Karlis <brahmaniskarlis@gmail.com>
2026-08-13 09:24:18 +0300

    Project initialization + supabase linking and setup

---
+++ ChooseLife/ChooseLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChooseLifeApp.swift
+//  ChooseLife
+//
+//  Created by karlis.brahmanis on 03/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct ChooseLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChooseLife/Supabase/Todo.swift
@@ -0,0 +1,13 @@
+//
+//  Todo.swift
+//  ChooseLife
+//
+//  Created by karlis.brahmanis on 03/08/2026.
+//
+
+import Foundation
+
+struct Todo: Identifiable, Decodable {
+  var id: Int
+  var name: String
+}
